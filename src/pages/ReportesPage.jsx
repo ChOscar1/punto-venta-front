928f764c55b887828abc79a385417210a6b19eca
ChOscar1 <oscarrrrrt@gmail.com>
2026-08-11 19:03:23 -0600

    corregir formato de fech

--- src/pages/ReportesPage.jsx
+++ src/pages/ReportesPage.jsx
@@ -7,7 +7,11 @@ function ReportePage() {
 
         const hoy = new Date()
 
-        return hoy.toISOString().split('T')[0]
+        const año = hoy.getFullYear()
+        const mes = String(hoy.getMonth() + 1).padStart(2, '0')
+        const dia = String(hoy.getDate()).padStart(2, '0')
+
+        return `${año}-${mes}-${dia}`
 
     })
 
